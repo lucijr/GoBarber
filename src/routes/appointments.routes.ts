3043e7cce81ea5228d08b22cca390fd092fb7ce6
lucijr <lucianoqazwsx@gmail.com>
2021-01-24 10:31:55 -0300

    chore: Scheduling Model. Created models folder  with Appointment.ts. Removed interface Appointment of appointments.routes.ts.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,16 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Appointment[] = [];
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
   const findAppointmentInSameDate = appointments.find(appointment =>
     isEqual(parsedDate, appointment.date),
   );
@@ -26,11 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked.' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
