0ee01857e52dc53637f769dddbc565df091a0387
lucijr <lucianoqazwsx@gmail.com>
2021-02-15 20:38:35 -0300

    create: 1613432276433-AddAvatarFieldToUsers.ts in src/database/migrations. This file is a migration, was created with command: yarn typeorm migration:create -n AddAvatarFieldToUsers.

---
+++ src/database/migrations/1613432276433-AddAvatarFieldToUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddAvatarFieldToUsers1613432276433 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
