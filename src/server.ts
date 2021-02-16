c565687f8b13f3ec2cad9aa4a37008a9c8b4948d
lucijr <lucianoqazwsx@gmail.com>
2021-02-16 02:36:43 -0300

    Update: server.ts Added router GET /files to display static avatar of users.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import 'reflect-metadata';
-import express, { request, response } from 'express';
+import express from 'express';
 import routes from './routes';
 
-import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
