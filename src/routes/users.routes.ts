fca8954f44e562624bbb97f64730611e83fed128
lucijr <lucianoqazwsx@gmail.com>
2021-02-15 23:27:55 -0300

    Update: upload.ts Configured patch /avatar to received file and save in server. Import multer and ../config/upload. Created const upload. Add middleware update.single('avatar') in router patch /avatar. Tested in Insomia
    PATCH: localhost:3333/users/avatar
    
    with body Multipart avatar _ Choose File , JWT and worked.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { request, Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -25,8 +29,15 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
