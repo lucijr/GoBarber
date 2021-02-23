917d009a82385fe5b82a0ef5d46595142b6d36cb
lucijr <lucianoqazwsx@gmail.com>
2021-02-23 08:41:57 -0300

    Update: AuthenticateUserService.ts Remove typeorm and User Import IUsersRepository and IUser Rename Request and Response to IRequest and IResponse Create constructor usersRepository. Update function execute

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,24 +1,24 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import authConfig from '@config/auth';
 import AppError from '@shared/error/AppError';
-import User from '../infra/typeorm/entities/User';
+import IUsersRespository from '../repositories/IUsersRepository';
+import IUser from '../entities/IUser';
 
-interface Request {
+interface IRequest {
   email: string;
   password: string;
 }
-interface Response {
-  user: User;
+interface IResponse {
+  user: IUser;
   token: string;
 }
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const userRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRespository) {}
 
-    const user = await userRepository.findOne({ where: { email } });
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email/password combination.', 401);
