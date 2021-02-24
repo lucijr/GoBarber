81195170b4d785755bdf671d319eeab9b3877788
lucijr <lucianoqazwsx@gmail.com>
2021-02-24 16:00:08 -0300

    Update: users.routes.ts Import container from tsrynge Remove UsersRepository and const usersRepository. Add container.resolve in const CreateUserService and const UpdateUserAvatarService.

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from 'express';
 import 'express-async-errors';
-
+import { container } from 'tsyringe';
 import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UptadeUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  const usersRepository = new UsersRepository();
-
   const { name, email, password } = request.body;
 
-  const createUserService = new CreateUserService(usersRepository);
+  const createUserService = container.resolve(CreateUserService);
 
   const user = await createUserService.execute({
     name,
@@ -35,9 +32,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-
-    const updateuserAvatar = new UptadeUserAvatarService(usersRepository);
+    const updateuserAvatar = container.resolve(UptadeUserAvatarService);
 
     const user = await updateuserAvatar.execute({
       user_id: request.user.id,
