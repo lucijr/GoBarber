68c1549f264c48780a18e09066cb80a653b0ba6d
lucijr <lucianoqazwsx@gmail.com>
2021-02-24 18:17:18 -0300

    Update: UsersController.ts Remove import UpdateUserAvatarService.

--- src/modules/users/infra/http/controllers/UsersContoller.ts
+++ src/modules/users/infra/http/controllers/UsersContoller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -14,22 +13,6 @@ export default class UsersController {
       name,
       email,
       password,
-    });
-
-    user.password = '';
-
-    return response.json(user);
-  }
-
-  public async updateAvatar(
-    request: Request,
-    response: Response,
-  ): Promise<Response> {
-    const updateuserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateuserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
     });
 
     user.password = '';
