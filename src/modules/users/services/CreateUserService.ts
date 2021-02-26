6d414bd9a259591575d00eb65ba64a0547f7152c
lucijr <lucianoqazwsx@gmail.com>
2021-02-26 07:18:17 -0300

    Update: CreateUserService.ts Removed bcrypt Imported IHashProvider Updated constructor and execute.

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,10 @@
-import { hash } from 'bcryptjs';
+import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/error/AppError';
 import IUsersRespository from '../repositories/IUsersRepository';
 import IUser from '../entities/IUser';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   name: string;
@@ -16,6 +17,9 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRespository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<IUser> {
@@ -25,7 +29,7 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
