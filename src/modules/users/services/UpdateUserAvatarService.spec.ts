f8962f855374196098f322ee412e8564acd576d2
lucijr <lucianoqazwsx@gmail.com>
2021-02-27 02:34:26 -0300

    Create: UpdateUserAvatarService.spec.ts This file its a test of update user avatar.

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,75 @@
+import AppError from '@shared/error/AppError';
+import FakeStorageProvider from '@shared/container/providers/StorageProviders/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateAvatarUser', () => {
+  it('should be able to create a new avatar in user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Luciano Júnior',
+      email: 'luciano@taragatech.com',
+      password: '12345678',
+    });
+    await updateUserAvatar.execute({
+      avatarFilename: 'user.jpg',
+      user_id: user.id,
+    });
+    expect(user.avatar).toBe('user.jpg');
+  });
+
+  it('should not be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        avatarFilename: 'user.jpeg',
+        user_id: 'non-existing-user',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'Luciano Júnior',
+      email: 'luciano@taragatech.com',
+      password: '12345678',
+    });
+
+    await updateUserAvatar.execute({
+      avatarFilename: 'avatar.jpg',
+      user_id: user.id,
+    });
+
+    await updateUserAvatar.execute({
+      avatarFilename: 'avatar2.jpg',
+      user_id: user.id,
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
+});
